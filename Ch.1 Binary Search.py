a941585f0f66668fefca22c723532c2ab5b06b66
jrwaheed <j.r.waheed@gmail.com>
2021-03-22 10:22:09 +0100

    Binary Search

---
+++ Ch.1 Binary Search.py
@@ -0,0 +1,21 @@
+##Binary search only works when your list is in sorted order. 
+
+def binarySearch(list, item):
+    low = 0
+    high = len(list) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+
+myList = [1,3,5,7,9]
+
+print(binarySearch(myList, 9))
